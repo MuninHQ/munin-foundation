268e1de2fe77d56198bc808a18f7b26706f9dd54
Munin Labs HQ <andresj.jardim@gmail.com>
2026-08-05 19:06:56 -0300

    feat: add M10.4 usability and launch readiness (#40)
    * feat: add single-command Munin launcher
    
    * feat: add one-command Munin startup
    
    * docs: add Munin workspace getting started guide

---
+++ scripts/launch.mjs
@@ -0,0 +1,42 @@
+import { spawn } from 'node:child_process';
+import { platform } from 'node:os';
+
+const children = [];
+
+function run(command, args) {
+  const child = spawn(command, args, {
+    stdio: 'inherit',
+    shell: platform() === 'win32',
+    env: process.env,
+  });
+  children.push(child);
+  child.on('exit', code => {
+    if (code && code !== 0) shutdown(code);
+  });
+  return child;
+}
+
+function openBrowser(url) {
+  const target = platform() === 'win32'
+    ? ['cmd', ['/c', 'start', '', url]]
+    : platform() === 'darwin'
+      ? ['open', [url]]
+      : ['xdg-open', [url]];
+  const child = spawn(target[0], target[1], { stdio: 'ignore', detached: true });
+  child.unref();
+}
+
+function shutdown(code = 0) {
+  for (const child of children) {
+    if (!child.killed) child.kill('SIGTERM');
+  }
+  process.exit(code);
+}
+
+process.on('SIGINT', () => shutdown(0));
+process.on('SIGTERM', () => shutdown(0));
+
+console.log('Starting Munin Workspace...');
+run('npm', ['run', 'api']);
+run('npm', ['run', 'web', '--', '--host', '127.0.0.1']);
+setTimeout(() => openBrowser('http://127.0.0.1:5173'), 2500);
