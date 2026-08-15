b4382c1ff25bc256285f47bd427092560bbd9688
Munin Labs HQ <andresj.jardim@gmail.com>
2026-08-15 19:04:55 -0300

    refresh mobile PWA cache for chat-first UX

--- apps/web/public/munin-sw.js
+++ apps/web/public/munin-sw.js
@@ -1,4 +1,4 @@
-const CACHE='munin-mobile-v1';
+const CACHE='munin-mobile-v2';
 const SHELL=['/mobile.html','/munin.webmanifest'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim();});
